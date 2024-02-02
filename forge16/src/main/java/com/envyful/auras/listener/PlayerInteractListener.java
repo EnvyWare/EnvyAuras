af03f7d19c38abbee0f8e737644d9a3858e20ffb
Daniel Norris <dnorris443@gmail.com>
2024-02-02 14:27:40 +0000

    feat(forge16): add application listener

---
+++ forge16/src/main/java/com/envyful/auras/listener/PlayerInteractListener.java
@@ -0,0 +1,39 @@
+package com.envyful.auras.listener;
+
+import com.envyful.auras.EnvyAuras;
+import com.pixelmonmod.pixelmon.entities.pixelmon.PixelmonEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.Hand;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class PlayerInteractListener {
+
+    @SubscribeEvent
+    public void onEntityInteract(PlayerInteractEvent.EntityInteractSpecific event) {
+        if (!(event.getTarget() instanceof PixelmonEntity)) {
+            return;
+        }
+
+        var pixelmon = (PixelmonEntity) event.getTarget();
+        var player = (ServerPlayerEntity) event.getPlayer();
+        var itemInHand = player.getItemInHand(Hand.MAIN_HAND);
+        var aura = EnvyAuras.getConfig().auraFromItem(itemInHand);
+
+        if (aura == null) {
+            return;
+        }
+
+        event.setCanceled(true);
+
+        if (!pixelmon.isOwnedBy(player)) {
+            //TODO: error + bypass logic
+            return;
+        }
+
+        pixelmon.getPokemon().getPersistentData().putString("ENVY_AURAS", aura.id());
+        //TODO: success message
+        itemInHand.shrink(1);
+    }
+
+}
